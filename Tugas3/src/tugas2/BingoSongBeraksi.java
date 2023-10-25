4921e1a475ae495f65ee35fb0a9d017cc877e1da
Hudzaifah Nabil Amrullah <114643052+Hudzaifah181104@users.noreply.github.com>
2023-10-25 23:41:37 +0700

    Delete Tugas3 directory

--- Tugas3/src/tugas2/BingoSongBeraksi.java
+++
@@ -1,9 +0,0 @@
-package tugas2;
-
-public class BingoSongBeraksi {
-    public static void main(String[] args){
-            BingoSong Tampilkan = new BingoSong();
-            
-            Tampilkan.TampilanBingo();
-    }
-}
--- Tugas3/src/tugas3/KonversiSuhu2.java
+++
@@ -1,8 +0,0 @@
-package tugas3;
-
-public class KonversiSuhu2 extends KonversiSuhu1 {
-    public double fahrenheitToReamur(double fahrenheit) {
-        double celsius = (fahrenheit - 32) * 5/9;
-        return celsius * 4/5;
-    }
-}
